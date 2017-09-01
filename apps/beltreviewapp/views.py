d24dd6d0ef267e13d2549190b4d10ab68fc94369
lex Phumirat <lexphumirat@gmail.com>
2017-08-31 22:09:07 -0700

    ver1.001

--- apps/beltreviewapp/views.py
+++ apps/beltreviewapp/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
 
-# Create your views here.
+
+
+
+
+def index(request):
+    return render(request , 'beltreviewapp/index.html')

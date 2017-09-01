951cf2c654515259ecef95c10694239f4c6e5206
lex Phumirat <lexphumirat@gmail.com>
2017-08-31 23:45:36 -0700

    ver1.003

--- apps/beltreviewapp/models.py
+++ apps/beltreviewapp/models.py
@@ -1,11 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import bcrypt
 from django.db import models
-
 from django.contrib import messages
 
 # Create your models here.
+class UserManger(models.Manager):
+    #send info over to views def register
+    def validate_reg(self, post_data):
+        errors = []
+        #username and alias must be 2 or more
+        if len(post_data['full_name']) < 2:
+            errors.append('username must be 2 or more characters')
+        if len(post_data['alias']) < 2:
+            errors.append('alias must be 2 or more characters')
+        #password must be 8 or more
+        #password must be the same as confirm
+        if len(post_data['password']) < 8:
+            errors.append('password must be 8 or more characters')
+        if len(post_data['password']) != len(post_data['confirmpw']):
+            errors.append('password must match')
+        return errors
+
+    def create_user(self, clean_data):
+        hashed = bcrypt.hashpw(clean_data['password'].encode(), bcrypt.gensalt())
+        return self.create(
+            full_name = clean_data['full_name'],
+            alias = clean_data['alias'],
+            email = clean_data['email'],
+            password = hashed,
+            dob = clean_data['dob']
+        )
+
 class User(models.Model):
     full_name = models.CharField(max_length=100, unique=True)
     alias = models.CharField(max_length=100, unique=True)
@@ -14,6 +40,7 @@ class User(models.Model):
     dob = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    objects = UserManger()
 
     def __str__(self):
         return self.full_name + " " + self.email
--- apps/beltreviewapp/views.py
+++ apps/beltreviewapp/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import User
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 
@@ -8,5 +8,15 @@ def index(request):
     return render(request , 'beltreviewapp/index.html')
 
 def register(request):
-    print request.POST
+    #gets info from UserManger
+    errs = User.objects.validate_reg(request.POST)
+    #if there are any errors from the errors[] list. it will not go through
+    if errs:
+        for e in errs:
+            messages.error(request, e)
+    else:
+        #make user if no errors
+        new_user = User.objects.create_user(request.POST)
+        request.session['id'] = new_user.id
+        messages.success(request, 'thank you {} for registering'.format(new_user.full_name))
     return redirect('/')
